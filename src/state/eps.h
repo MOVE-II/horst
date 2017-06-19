e0b1903009b478471e9cb7b95ce15ab0d20b90ca
Alexander Lill <alexander.lill@tum.de>
2017-06-20 01:11:49 +0200

    Remove unnecessary code for S-Band support

--- src/state/eps.h
+++ src/state/eps.h
@@ -20,18 +20,6 @@ public:
 	std::vector<std::unique_ptr<Action>> transform_to(const EPS &target) const;
 
 	/**
-	 * Power state of sband.
-	 * The WAIT_* states are used to store that the switch
-	 * was requested but not yet confirmed.
-	 */
-	enum class sband_state {
-		OFF,
-		WAIT_OFF,
-		ON,
-		WAIT_ON
-	} sband;
-
-	/**
 	 * battery level in [0.01]% (that means 1000 => 10% battery left)
 	 */
 	uint16_t battery_level;
